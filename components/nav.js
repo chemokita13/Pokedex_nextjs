8d1727c85a8a64feb1c593f0f929a7f17a4d7d42
chemokita13 <87540108+chemokita13@users.noreply.github.com>
2022-11-05 21:04:38 +0000

    navigation customized

--- components/nav.js
+++ components/nav.js
@@ -2,23 +2,33 @@ import Link from "next/link" // routes
 
 function Navigation() {
     return (
-        <ul>
-            <li>
-                <Link href="/" legacyBehavior>
-                    <a>Index</a>
-                </Link>
-            </li>
-            <li>
-                <Link href="/pokemon" legacyBehavior>
-                    <a>Pokemon</a>
-                </Link>
-            </li>
-            <li>
-                <Link href="/others" legacyBehavior>
-                    <a>Others</a>
-                </Link>
-            </li> 
-        </ul>
+        <header className="nav-header">
+            <div className="nav-logo">
+                <img className='nav-logo-img' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRsbReYytwbpM15Ml3vPTItOr8axh16IsmxqA&usqp=CAU" />
+            </div>
+            <nav>
+                <ul className="nav-links">
+                    <li>
+                        <Link href="/" legacyBehavior>
+                            <a>Index</a>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="/pokemon" legacyBehavior>
+                            <a>Pokemon</a>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="/others" legacyBehavior>
+                            <a>Others</a>
+                        </Link>
+                    </li>
+                </ul>
+            </nav>
+            <a href="" className="btn">
+                <button>Github</button>
+            </a>
+        </header>
     )
 }
 
--- components/pokemonInfo.js
+++ components/pokemonInfo.js
@@ -3,7 +3,7 @@ import Link from "next/link"
 function PokemonInfo({ pokemon }) {
 
   return (
-    <div>
+    <div key={pokemon.entry_number}>
       <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.entry_number}.png`} />
       <h1>{pokemon.entry_number}</h1>,
       <h2>{pokemon.pokemon_species.name}</h2>
